35a4c5b0bdf25e68684fd99ad08538e1d93f519c
Alex Mercer <inteleva12@gmail.com>
2022-05-31 09:31:29 +0300

    edit time

--- links.js
+++ links.js
@@ -21,27 +21,27 @@ async function send_link(link, password = ''){
 }
 
 // Monday
-schedule.scheduleJob('55 7 * * 1', () => send_link(config.informatics));
+schedule.scheduleJob('25 9 * * 1', () => send_link(config.informatics));
 schedule.scheduleJob('15 11 * * 1', () => send_link(config.mathematics));
 schedule.scheduleJob('45 12 * * 1', () => send_link(config.algebra));
 
 // Tuesday
-schedule.scheduleJob('55 7 * * 2', () => send_link(config.discrete_math));
+schedule.scheduleJob('25 9 * * 2', () => send_link(config.discrete_math));
 schedule.scheduleJob('15 11 * * 2', () => send_link(config.discrete_math));
 schedule.scheduleJob('45 12 * * 2', () => send_link(config.english));
 
 // Wednesday
-// schedule.scheduleJob('55 7 * * 3', () => send_link(config.history));
+// schedule.scheduleJob('25 9 * * 3', () => send_link(config.history));
 schedule.scheduleJob('15 11 * * 3', () => send_link(config.history));
 schedule.scheduleJob('45 12 * * 3', () => send_link(config.protection_of_ukraine, config.protection_of_ukraine_password));
 
 // Thursday
-schedule.scheduleJob('55 7 * * 4', () => send_link(config.physical_education));
+schedule.scheduleJob('25 9 * * 4', () => send_link(config.physical_education));
 schedule.scheduleJob('15 11 * * 4', () => send_link(config.foreign_literature));
 schedule.scheduleJob('45 12 * * 4', () => send_link(config.economic_theory));
 
 // Friday
-schedule.scheduleJob('55 7 * * 5', () => send_link(config.chemistry));
+schedule.scheduleJob('25 9 * * 5', () => send_link(config.chemistry));
 schedule.scheduleJob('15 11 * * 5', () => send_link(config.mathematics));
 // schedule.scheduleJob('45 12 * * 5', () => send_link(config.));
 
