b0d8a0a54fa095ed7b782f88350080b0acacdef2
Oleksandr Havrilursa <alex.mercer.324110@gmail.com>
2022-08-31 17:27:25 +0300

    A?

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const TelegramBot = require("node-telegram-bot-api");
 const schedule = require("node-schedule");
 const fs = require("fs");
-const { send } = require("process");
 
 process.env.TZ = "Europe/Kiev";
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_API_TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_API_TOKEN, {
+  polling: true,
+});
 
 const ALEX_CHAT_ID = 961384484;
 const TEST_GROUP_ID = -1001551171870;
 const MATH_GROUP_ID = -1001520763217;
-const TARGET_GROUP = TEST_GROUP_ID;
+const TARGET_GROUP = MATH_GROUP_ID;
 
 bot.on("message", (message) => {
   if (message.chat.id === ALEX_CHAT_ID) {
@@ -25,10 +26,12 @@ bot.on("message", (message) => {
 });
 
 async function sendMessage(link, password = "") {
-  const sendedMessage = await bot.sendMessage(TARGET_GROUP, link, { parse_mode: 'MarkDown' });
+  const sendedMessage = await bot.sendMessage(TARGET_GROUP, link, {
+    parse_mode: "MarkDown",
+  });
 
   if (password) {
-    bot.sendMessage(TARGET_GROUP, password)
+    bot.sendMessage(TARGET_GROUP, password);
   }
 }
 
@@ -40,9 +43,13 @@ function check() {
       const [hour, minute] = time.split(":");
       const mySchedule = `${minute} ${hour} * * ${day}`;
 
-      const date = new Date()
-      if (date.getHours() == hour && date.getMinutes() == minute && date.getDay() == day) {
-        sendMessage(link)
+      const date = new Date();
+      if (
+        date.getHours() == hour &&
+        date.getMinutes() == minute &&
+        date.getDay() == day
+      ) {
+        sendMessage(link);
       }
     }
   }
