a0d1c520b01919162515d32fcb9a9ef515fbf6d5
Oleksandr Havrilutsa <alex.mercer.324110@gmail.com>
2022-12-06 12:28:40 +0200

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const schedule = require("node-schedule");
-const fs = require("fs");
+const fetch = require('node-fetch');
 
 process.env.TZ = "Europe/Kiev";
 
@@ -18,18 +18,20 @@ function sendMessage(link) {
 }
 
 function check() {
-  const config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
+  fetch('https://raw.githubusercontent.com/AlexMercer324110/NodeJS_telegram_bot/master/config.json')
+    .then(res => res.json())
+    .then(json => {
+      for (const [day, schedule] of Object.entries(json)) {
+        for (const [time, link] of Object.entries(schedule)) {
+          const [hour, minute] = time.split(":");
+          const date = new Date();
 
-  for (const [day, schedule] of Object.entries(config)) {
-    for (const [time, link] of Object.entries(schedule)) {
-      const [hour, minute] = time.split(":");
-      const date = new Date();
-
-      if (date.getHours() === +hour && date.getMinutes() === +minute && date.getDay() === +day && link) {
-        sendMessage(link);
+          if (date.getHours() === +hour && date.getMinutes() === +minute && date.getDay() === +day && link) {
+            sendMessage(link);
+          }
+        }
       }
-    }
-  }
+    })
 }
 
-schedule.scheduleJob("1 * * * * *", check);
+schedule.scheduleJob("50 * * * * *", check);
