e980ed6905338b2c421c9d4a7104ab800b0f2589
Oleksandr Havrilutsa <alex.mercer.324110@gmail.com>
2022-12-19 22:48:42 +0200

    a

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-const TelegramBot = require("node-telegram-bot-api");
-const schedule = require("node-schedule");
-const fetch = require('node-fetch');
+import TelegramBot from 'node-telegram-bot-api'
+import fetch from 'node-fetch'
 
-process.env.TZ = "Europe/Kiev";
+process.env.TZ = "Europe/Kiev"
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_API_TOKEN, {
   polling: true,
-});
+})
 
-const MATH_GROUP_ID = -1001520763217;
-const TARGET_GROUP = MATH_GROUP_ID;
+const math_group_id = -1001520763217
+const nbk_group_id = -1001525763156
 
-function sendMessage(link) {
-  const sendedMessage = bot.sendMessage(TARGET_GROUP, link, {
-    parse_mode: "MarkDown",
-  });
+const fetchSchedule = async () => {
+  const res = await fetch('https://raw.githubusercontent.com/AlexMercer324110/NodeJS_telegram_bot/master/config.json', {cache: 'no-cache'})
+  return await res.json()
 }
 
-function check() {
-  fetch('https://raw.githubusercontent.com/AlexMercer324110/NodeJS_telegram_bot/master/config.json', {cache: 'no-cache'})
-    .then(res => res.json())
-    .then(json => {
-      for (const [day, schedule] of Object.entries(json)) {
-        for (const [time, link] of Object.entries(schedule)) {
-          const [hour, minute] = time.split(":");
-          const date = new Date();
+setInterval(async () => {
+  const json = await fetchSchedule()
 
-          if (date.getHours() === +hour && date.getMinutes() === +minute && date.getDay() === +day && link) {
-            sendMessage(link);
-          }
-        }
+  for (const [day, schedule] of Object.entries(json)) {
+    for (const [time, link] of Object.entries(schedule)) {
+      const [hour, minute] = time.split(":")
+      const date = new Date()
+
+      if (date.getHours() === +hour && date.getMinutes() === +minute && date.getDay() === +day && link) {
+        bot.sendMessage(math_group_id, link)
+        bot.sendMessage(nbk_group_id, link)
       }
-    })
-}
+    }
+  }
+}, 1000)
 
-schedule.scheduleJob("1 * * * * *", check);
+console.log('Bomb has been planted')
