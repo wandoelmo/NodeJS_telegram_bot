1fcf9e3a1a8404aaa4a93ead2237716c009d00dc
Oleksandr Havrilutsa <alex.mercer.324110@gmail.com>
2022-12-20 13:02:03 +0200

    deleted nbk group

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_API_TOKEN, {
 })
 
 const math_group_id = -1001520763217
-const nbk_group_id = -1001525763156
 
 const fetchSchedule = async () => {
   const res = await fetch('https://raw.githubusercontent.com/AlexMercer324110/NodeJS_telegram_bot/master/config.json', {cache: 'no-cache'})
@@ -25,8 +24,7 @@ setInterval(async () => {
 
       if (date.getHours() === +hour && date.getMinutes() === +minute && date.getDay() === +day && message) {
         bot.sendMessage(math_group_id, message)
-        bot.sendMessage(nbk_group_id, message)
-        console.log(message)
+        console.log('\n', message, '\n')
       }
     }
   }
