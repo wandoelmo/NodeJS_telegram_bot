b55c5f6b3fbf0b60938a96dc6f992ce51e9a3ae5
Oleksandr Havrilutsa <alex.mercer.324110@gmail.com>
2023-01-03 16:44:56 +0200

    format

--- bot.js
+++ bot.js
@@ -1,32 +1,35 @@
-import TelegramBot from 'node-telegram-bot-api'
+import TelegramBot from "node-telegram-bot-api";
 import { fetchUsers } from "./fetch.js";
 
-const admin_chat_id = [961384484, 840090351]
+const admin_chat_id = [961384484, 840090351];
 
 // setup bot
 export const bot = new TelegramBot(process.env.TELEGRAM_BOT_API_TOKEN, {
   polling: true,
-})
+});
 
 // commands
-bot.on('message', async msg => {
-  if (!msg.text)
-    return
-  if (msg.chat.type === 'group' || msg.chat.type === 'supergroup') {
-    if (msg.text.includes('@all') && admin_chat_id.includes(msg.from.id)) {
-      const users = await fetchUsers()
-      let all = ''
+bot.on("message", async (msg) => {
+  if (!msg.text) return;
+  if (msg.chat.type === "group" || msg.chat.type === "supergroup") {
+    if (msg.text.includes("@all") && admin_chat_id.includes(msg.from.id)) {
+      const users = await fetchUsers();
+      let all = "";
 
       for (const user_id of users) {
-        all += `<a href="tg://user?id=${user_id}">.</a>`
+        all += `<a href="tg://user?id=${user_id}">.</a>`;
       }
 
-      await bot.deleteMessage(msg.chat.id, msg.message_id).catch(() => {})
-      await bot.sendMessage(msg.chat.id, msg.text.replace('@all', all), {parse_mode: 'HTML'}).catch(() => {})
+      await bot.deleteMessage(msg.chat.id, msg.message_id).catch(() => {});
+      await bot
+        .sendMessage(msg.chat.id, msg.text.replace("@all", all), {
+          parse_mode: "HTML",
+        })
+        .catch(() => {});
     }
-  } else if (msg.chat.type === 'private') {
-    if (msg.text !== '/start') {
-      await bot.deleteMessage(msg.chat.id, msg.message_id).catch(() => {})
+  } else if (msg.chat.type === "private") {
+    if (msg.text !== "/start") {
+      await bot.deleteMessage(msg.chat.id, msg.message_id).catch(() => {});
     }
   }
-})
+});
--- fetch.js
+++ fetch.js
@@ -1,17 +1,19 @@
 import fetch from "node-fetch";
 
-const admin_chat_id = 961384484
+const admin_chat_id = 961384484;
 
 export const fetchSchedule = async () => {
-  const scheduleUrl = 'https://raw.githubusercontent.com/oleksandrh324110/NodeJS_telegram_bot/master/schedule.json'
+  const scheduleUrl =
+    "https://raw.githubusercontent.com/oleksandrh324110/NodeJS_telegram_bot/master/schedule.json";
 
-  const res = await fetch(scheduleUrl, {cache: 'no=cache'})
-  return await res.json()
-}
+  const res = await fetch(scheduleUrl, { cache: "no=cache" });
+  return await res.json();
+};
 
 export const fetchUsers = async () => {
-  const usersUrl = 'https://raw.githubusercontent.com/oleksandrh324110/NodeJS_telegram_bot/master/users.json'
+  const usersUrl =
+    "https://raw.githubusercontent.com/oleksandrh324110/NodeJS_telegram_bot/master/users.json";
 
-  const res = await fetch(usersUrl, {cache: 'no-cache'})
-  return await res.json()
-}
+  const res = await fetch(usersUrl, { cache: "no-cache" });
+  return await res.json();
+};
