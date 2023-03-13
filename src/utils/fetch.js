83013ddb7c305b0f6d8563aca04c84c3130a2cff
Oleksandr Havrilutsa <alex.mercer.324110@gmail.com>
2023-03-13 20:30:11 +0200

    hwat

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,17 +1,17 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 
 export const fetchSchedule = async () => {
   const scheduleUrl =
-    "https://raw.githubusercontent.com/oleksandrh324110/NodeJS_telegram_bot/master/schedule.json";
+    'https://raw.githubusercontent.com/oleksandrh324110/NodeJS_telegram_bot/master/schedule.json'
 
-  const res = await fetch(scheduleUrl, { cache: "no=cache" });
-  return (await res.json().catch(console.error)) ?? [];
-};
+  const res = await fetch(scheduleUrl, { cache: 'no=cache' })
+  return await res.json().catch(console.error)
+}
 
 export const fetchUsers = async () => {
   const usersUrl =
-    "https://raw.githubusercontent.com/oleksandrh324110/NodeJS_telegram_bot/master/users.json";
+    'https://raw.githubusercontent.com/oleksandrh324110/NodeJS_telegram_bot/master/users.json'
 
-  const res = await fetch(usersUrl, { cache: "no-cache" });
-  return (await res.json().catch(console.error)) ?? [];
-};
+  const res = await fetch(usersUrl, { cache: 'no-cache' })
+  return await res.json().catch(console.error)
+}
--- src/utils/sendSchedule.js
+++ src/utils/sendSchedule.js
@@ -1,31 +1,28 @@
-import { fetchSchedule } from "../utils/fetch.js";
-import { fetchUsers } from "../utils/fetch.js";
-import { bot } from "../bot.js";
+import { bot } from '../bot.js'
+import { fetchSchedule, fetchUsers } from '../utils/fetch.js'
 
 setInterval(async () => {
-  const json = await fetchSchedule();
+  const json = await fetchSchedule()
 
-  if (!json) return;
+  if (!json) return
 
   for (const [day, schedule] of Object.entries(json)) {
     for (const [time, message] of Object.entries(schedule)) {
-      const [hour, minute] = time.split(":").map((v) => +v);
-      const date = new Date();
+      const [hour, minute] = time.split(':').map((v) => +v)
+      const date = new Date()
 
       if (
         date.getHours() === hour &&
         date.getMinutes() === minute &&
-        date.getDay() === day &&
+        date.getDay() === +day &&
         message
       ) {
-        const users = await fetchUsers();
-
-        for (let user_id of users) {
-          bot.sendMessage(user_id, message).catch(() => {});
+        for (let user_id of await fetchUsers()) {
+          bot.sendMessage(user_id, message).catch(console.error)
         }
 
-        console.log("\n", message, "\n");
+        console.log('\n', message, '\n')
       }
     }
   }
-}, 1000 * 60);
+}, 1000 * 60)
